6eca09c53574d5293f5a8c52cd00626dbf682290
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 04:25:33 +0300

    Add ContactsScreenVC

---
+++ CustomTabBar/CustomTabBar/ContactsScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsScreenVC.swift
+//  CustomTabBar
+//
+//  Created by Арсентий Халимовский on 17.08.2023.
+//
+
+import UIKit
+
+final class ContactsScreenVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
