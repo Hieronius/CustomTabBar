eb8529eb3e91cbad04da8c7bc9cd3d83799ad5ea
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 04:27:08 +0300

    Add BasketScreenVC

---
+++ CustomTabBar/CustomTabBar/BasketScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  BasketScreenVC.swift
+//  CustomTabBar
+//
+//  Created by Арсентий Халимовский on 17.08.2023.
+//
+
+import UIKit
+
+final class BasketScreenVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
