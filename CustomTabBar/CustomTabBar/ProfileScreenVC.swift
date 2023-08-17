aa33e01d0e649e33229396f234e75838111d5f44
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 04:23:23 +0300

    Add ProfileScreenVC

---
+++ CustomTabBar/CustomTabBar/ProfileScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileScreenVC.swift
+//  CustomTabBar
+//
+//  Created by Арсентий Халимовский on 17.08.2023.
+//
+
+import UIKit
+
+final class ProfileScreenVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
