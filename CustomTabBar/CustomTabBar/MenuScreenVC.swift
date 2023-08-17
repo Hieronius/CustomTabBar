0ef629c46bee0a93f5f22483eaf8071eac79b0c7
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 04:19:43 +0300

    Add MenuScreenVC

---
+++ CustomTabBar/CustomTabBar/MenuScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  CustomTabBar
+//
+//  Created by Арсентий Халимовский on 17.08.2023.
+//
+
+import UIKit
+
+final class MenuScreenVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
