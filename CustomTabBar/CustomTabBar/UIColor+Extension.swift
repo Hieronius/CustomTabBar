ae62ed3b8d7e8c45dc676c5511cb225461c9641d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 05:03:15 +0300

    Add Color extension

---
+++ CustomTabBar/CustomTabBar/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension.swift
+//  CustomTabBar
+//
+//  Created by Арсентий Халимовский on 17.08.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var tabBarItemAccent: UIColor {
+        #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    }
+    static var mainWhite: UIColor {
+        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    static var tabBarItemLight: UIColor {
+        #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 0.5111554885)
+    }
+}
