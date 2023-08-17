59e1038ca2ad580aeea55043c07f31916049b453
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 04:58:28 +0300

    Add custom shape for tab bar

--- CustomTabBar/CustomTabBar/MainTabBarViewController.swift
+++ CustomTabBar/CustomTabBar/MainTabBarViewController.swift
@@ -14,6 +14,7 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBar()
+        setTabBarAppearance()
     }
     
     // MARK: - Private Methods
@@ -31,6 +32,29 @@ class MainTabBarViewController: UITabBarController {
         viewController.tabBarItem.title = title
         viewController.tabBarItem.image = image
         return viewController
+    }
+    
+    private func setTabBarAppearance() {
+        let positionOnX: CGFloat = 10
+        let positionOnY: CGFloat = 14
+        let width = tabBar.bounds.width - positionOnX * 2
+        let height = tabBar.bounds.height + positionOnY * 2
+        
+        let roundLayer = CAShapeLayer()
+        
+        let bezierPath = UIBezierPath(
+            roundedRect: CGRect(x: positionOnX,
+                                y: tabBar.bounds.minY - positionOnY,
+                                width: width,
+                                height: height),
+                                cornerRadius: height / 2)
+        
+        roundLayer.path = bezierPath.cgPath
+        
+        tabBar.layer.insertSublayer(roundLayer, at: 0)
+        
+        tabBar.itemWidth = width / 5
+        tabBar.itemPositioning = .centered
     }
 
 
