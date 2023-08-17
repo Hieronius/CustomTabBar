88518e2012f9dc64445bace934c76e67bf0d5ae1
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 04:35:53 +0300

    Add function to configure a single VC

--- CustomTabBar/CustomTabBar/MainTabBarViewController.swift
+++ CustomTabBar/CustomTabBar/MainTabBarViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupVC(viewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+        viewController.tabBarItem.title = title
+        viewController.tabBarItem.image = image
+        return viewController
     }
 
 
