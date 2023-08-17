5625b88ddd02d42bbed397f2e485b7859547a547
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-17 04:49:27 +0300

    Change the size of tab bar icons

--- CustomTabBar/CustomTabBar/MainTabBarViewController.swift
+++ CustomTabBar/CustomTabBar/MainTabBarViewController.swift
@@ -13,7 +13,7 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTabBar()
     }
     
     // MARK: - Private Methods
